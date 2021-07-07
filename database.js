3aa0d34205798dd8e53a5f0877a19004305b85d0
gavkmr2001 <68558579+gavkmr2001@users.noreply.github.com>
2021-07-07 21:13:19 +0530

    unable to connect to backend

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const connection = "mongodb+srv://shopping-frontend:Gaurav@94310@shopping-frontend.065r7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const connection = process.env.DATABASEURL
 mongoose.connect(connection,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
     .then(() => console.log("Database Connected Successfully"))
     .catch(err => console.log(err));
