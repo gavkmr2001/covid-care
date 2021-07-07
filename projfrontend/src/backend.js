4efa9b8116d8a8560cc1b6c188c2423185024927
gavkmr2001 <68558579+gavkmr2001@users.noreply.github.com>
2021-07-07 23:59:18 +0530

    unable to connect to backend

--- projfrontend/src/backend.js
+++ projfrontend/src/backend.js
@@ -1,2 +1,2 @@
-// export const API = process.env.REACT_APP_BACKEND;
-export const API = process.env.PORT;
+export const API = process.env.REACT_APP_BACKEND;
+// export const API = process.env.PORT;
